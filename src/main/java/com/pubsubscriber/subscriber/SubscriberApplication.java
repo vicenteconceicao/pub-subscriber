fea351d81e041765c2fd2e00f4d1eeb4d5bc7ec7
Vicente Jr <vicente.junior@live.com>
2021-07-24 19:28:34 -0300

    Split Publisher and Subscriber application

--- src/main/java/com/pubsubscriber/subscriber/SubscriberApplication.java
+++ src/main/java/com/pubsubscriber/subscriber/SubscriberApplication.java
@@ -1,5 +1,6 @@
 package com.pubsubscriber.subscriber;
 
+import com.pubsubscriber.subscriber.services.GoogleSubscriberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class SubscriberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriberApplication.class, args);
+		GoogleSubscriberService.subscribeAsync();
 	}
 
 }
